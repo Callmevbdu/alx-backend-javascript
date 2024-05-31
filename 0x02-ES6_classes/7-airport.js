46f990b258da7d45b6713f833eaca0939a6789a1
Abdellatif <abdelt.2012@gmail.com>
2024-05-31 02:15:58 +0100

    Done.

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
