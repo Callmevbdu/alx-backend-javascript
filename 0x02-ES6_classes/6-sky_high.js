0b971ba06bb4a653cc76b8251d1d2adb7ff0a38f
Abdellatif <abdelt.2012@gmail.com>
2024-05-31 02:06:54 +0100

    Done.

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
