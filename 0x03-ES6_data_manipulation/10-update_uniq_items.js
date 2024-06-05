588fd294e64580910683bac934765ea73b0629a0
Abdellatif <abdelt.2012@gmail.com>
2024-06-05 03:44:42 +0100

    Done.

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -9,14 +9,12 @@
 
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw Error('Cannot process');
+    throw new Error('Cannot process');
   }
   
-  for (const item of map) {
-    if (item[1] === 1) {
-      map.set(item[0], 100);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
-  }
-  
-  return map;
+  });
 }
