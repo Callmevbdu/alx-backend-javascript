6d4dd2d8272ef92efef6ade3f8c370c706c0d6f8
Abdellatif <abdelt.2012@gmail.com>
2024-06-05 01:21:23 +0100

    Done.

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,13 @@
+/**
+ * a function getStudentsByLocation that returns an array of objects who are
+ * located in a specific city. It should accept a list of students
+ * (from getListStudents) and a city (string) as parameters. You must use the
+ * filter function on the array.
+ */
+
+export default function getStudentsByLocation(list, city) {
+  if (list instanceof Array) {
+    return list.filter((student) => student.location === city);
+  }
+  return [];
+}
