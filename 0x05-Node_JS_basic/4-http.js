098ff9678800eeb7e173eb8a44cb4f537d6a11db
Abdellatif <abdelt.2012@gmail.com>
2024-07-24 02:22:49 +0100

    Done.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,3 @@
-/**
- * Create a small HTTP server using the http module:
- * 	- It should be assigned to the variable app and this one must be
- * 	exported
- * 	- HTTP server should listen on port 1245
- * 	- Displays Hello Holberton School! in the page body for any endpoint
- * 	as plain text
- */
 const http = require('http');
 
 const app = http.createServer((req, res) => {
