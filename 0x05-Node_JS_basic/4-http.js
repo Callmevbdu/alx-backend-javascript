d741aac14cbd0abf71aef29a088f1b266afee536
Abdellatif <abdelt.2012@gmail.com>
2024-07-24 02:30:15 +0100

    Done.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,27 @@
+/**
+ * Create a small HTTP server using the http module:
+ *      - It should be assigned to the variable app and this one must be
+ *      exported
+ *      - HTTP server should listen on port 1245
+ *      - Displays Hello Holberton School! in the page body for any endpoint
+ *      as plain text
+ */
 const http = require('http');
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer()
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
+app.on('request', (_, res) => {
+    const responseText = 'Hello Holberton School!';
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', responseText.length);
+    res.statusCode = 200;
+    res.write(Buffer.from(responseText));
 });
 
-app.listen(1245, () => {
-    console.log('Server listening on port 1245');
+app.listen(PORT, HOST, () => {
+    process.stdout.write('Server listening at -> http://${HOST}:${PORT}\n');
 });
 
 module.exports = app;
