540ed625b0158f94a091a3d617201ae074871117
Abdellatif <abdelt.2012@gmail.com>
2024-07-26 23:12:04 +0100

    Done.

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,3 @@
-/**
- * a function named calculateNumber. It should accepts two arguments (number)
- * a and b. The function should round a and b and return the sum of it
- */
 function calculateNumber(a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
